46b0560a7bbca4f9c991a25c0e45c60533f6c986
nova-sangeeth <novasangeeth@outlook.com>
2020-03-13 00:22:36 +0530

    added delete functionality

--- src/emp_app/urls.py
+++ src/emp_app/urls.py
@@ -3,8 +3,10 @@ from django.conf.urls import url
 from .views import *
 urlpatterns = [
     url(r'^$', index, name="index"),
-    url(r'^addemployee$', addemployee, name="addemployee"),
+    url(r'^employeeform$', employee_form, name="employee_form"),
+    url(r'^employeeform/(?P<id>\d+)/$', employee_form, name="employee_update"),
     # url(r'^editemployee/(?P<pk>\d+)/$', addemployee, name='editemployee'),
-    url(r'^employee_list$', employee_list, name="employee_list"),
-
+    url(r'^employeelist$', employee_list, name="employee_list"),
+    url(r'^delete/(?P<id>\d+)/$',
+        employee_delete, name="employee_delete")
 ]
--- src/emp_app/views.py
+++ src/emp_app/views.py
@@ -9,8 +9,8 @@ def index(request):
 
 
 def employee_list(request):
-    content = {'employee_list': Employee.objects.all()}
-    return render(request, "emp_list.html", content)
+    context = {'employee_list': Employee.objects.all()}
+    return render(request, "employee_list.html", context)
 
 # Try the inherited method like the inventory...
 
@@ -22,7 +22,7 @@ def employee_form(request, id=0):
         else:
             employee = Employee.objects.get(pk=id)
             form = EmployeeForm(instance=employee)
-        return render(request, "emp_form.html", {'form': form})
+        return render(request, "employee_form.html", {'form': form})
     else:
         if id == 0:
             form = EmployeeForm(request.POST)
@@ -31,10 +31,10 @@ def employee_form(request, id=0):
             form = EmployeeForm(request.POST, instance=employee)
         if form.is_valid():
             form.save()
-        return redirect('/employee/list')
+        return redirect('employee_list')
 
 
 def employee_delete(request, id):
-    employee = Employee.objects.get(pk=id)
-    employee.delete()
-    return redirect('/employee/list')
+    employee = Employee.objects.get(pk=id).delete()
+    # employee.delete()
+    return redirect('employee_list')
